e7f047ff809c9d24b27a72d1e73ee911c9d40b0e
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2024-04-29 10:44:58 -0700

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,43 @@
 #include "sort.h"
 
-void swap_ints(int *a, int *b)
-{
-        int tmp;
+/**
+ * ints_swap - This function performs swapping operation
+ * @a: The first integer
+ * @b: The second integer
+ * Return: None
+ */
 
-        tmp = *a;
-        *a = *b;
-        *b = tmp;
+void ints_swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
+/**
+ * bubble_sort - This function performs the bubble sorting algo
+ * @array: The array to be sorted
+ * @size: The size of the array
+ * Return: None
+ */
 
+void bubble_sort(int *array, size_t size)
+{
 
-void bubble_sort(int *array, size_t size) {
+	if (size < 2 || array == NULL)
+		return;
 
-    if (size < 2 || array == NULL)
-            return;
-
-    for (size_t i = 0; i < size - 1; i++) {
-        for (size_t j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap_ints(array + j, array + j + 1);
-                print_array(array, size);
-            }
-        }
-    }
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		for (size_t j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				ints_swap(array + j, array + j + 1);
+				print_array(array, size);
+			}
+		}
+	}
 }
